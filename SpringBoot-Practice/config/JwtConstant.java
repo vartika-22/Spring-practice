1476cf6453a4f7d223e8132cfcef4bdd889fab2f
vartika-22 <112158995+vartika-22@users.noreply.github.com>
2023-12-29 10:31:23 +0530

    Add files via upload

---
+++ SpringBoot-Practice/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.spring.log.config;
+
+public class JwtConstant {
+
+	public static String JWT_HEADER="Authorization";
+	public static String SECRET_KEY="hhihhihjkgvvhhgcghcgctefksjkbvjkbvucigyig";
+}
