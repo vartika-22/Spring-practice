ed5935db09510c034d5807cc59a0f2149fec6fbb
vartika-22 <112158995+vartika-22@users.noreply.github.com>
2024-01-01 10:38:18 +0530

    Update SpringBootPracticeApplication.java

--- SpringBoot-Practice/SpringBootPracticeApplication.java
+++ SpringBoot-Practice/SpringBootPracticeApplication.java
@@ -1,13 +1,15 @@
-package com.springboot.practic;
+package com.spring.log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringBootPracticeApplication {
+@ComponentScan(basePackages = "com.spring.log")
+public class SpringSocialMediaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPracticeApplication.class, args);
+		SpringApplication.run(SpringSocialMediaApplication.class, args);
 	}
 
 }
