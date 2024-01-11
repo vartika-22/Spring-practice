33b9c7883ef6261c3528e62fb94940dcd4f6e4a3
vartika-22 <112158995+vartika-22@users.noreply.github.com>
2024-01-11 18:52:19 +0530

    Update User.java
    added variable for profile image

--- SpringBoot-Practice/model/User.java
+++ SpringBoot-Practice/model/User.java
@@ -1,47 +1,135 @@
-package com.springboot.practic.model;
+package com.spring.log.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Data
-@Table(name="User")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "Users")
 public class User {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	Integer id;
-	@Column
-	String firstName;
-	String lastName;
-	@Column(nullable = false,unique = true)
-	String email;
-	String password;
-	String gender;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "First_name" ,nullable = false)
+	private String firstName;
+	@Column(name = "last_name",nullable = false)
+	private String lastName;
+	@Column(unique = true,nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	private String imageUrl;
+
+	private String gender;
 	private List<Integer> followers=new ArrayList<>();
 	private List<Integer> followings=new ArrayList<>();
-	//
 	@JsonIgnore
 	@ManyToMany
 	private List<Post> savedPost =new ArrayList<>();
-	
 
-	
+	public User() {
+
+	}
+	public User(Integer id, String firstName, String lastName, String email, String password, String imageUrl,
+			String gender, List<Integer> followers, List<Integer> followings, List<Post> savedPost) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.imageUrl = imageUrl;
+		this.gender = gender;
+		this.followers = followers;
+		this.followings = followings;
+		this.savedPost = savedPost;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public List<Integer> getFollowers() {
+		return followers;
+	}
+
+	public void setFollowers(List<Integer> followers) {
+		this.followers = followers;
+	}
+
+	public List<Integer> getFollowings() {
+		return followings;
+	}
+
+	public void setFollowings(List<Integer> followings) {
+		this.followings = followings;
+	}
+
+	public List<Post> getSavedPost() {
+		return savedPost;
+	}
+
+	public void setSavedPost(List<Post> savedPost) {
+		this.savedPost = savedPost;
+	}
 
 }
